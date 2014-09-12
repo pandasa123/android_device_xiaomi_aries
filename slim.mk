9b7ec7667ad1d7b394e749f4f0ce46999ec59437
Sanket Panda <pandasa123@gmail.com>
2014-09-12 17:04:32 -0400

    Update and rename cm.mk to slim.mk

--- slim.mk
+++ slim.mk
@@ -3,14 +3,14 @@ TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/xiaomi/aries/full_aries.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := aries
-PRODUCT_NAME := cm_aries
+PRODUCT_NAME := slim_aries
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI 2
 PRODUCT_MANUFACTURER := XIAOMI
